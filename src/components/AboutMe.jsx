97d9605607b4d5fae222e1b3f009fc5e4ec12188
Rama.ui661 <Ramap.ui661@gmail.com>
2020-08-06 08:29:05 -0700

    Added content

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -5,7 +5,7 @@ export default function AboutMe () {
        <div className='plr mtb bg-color'>
                 <h1 className="text-center ">About Me</h1>
                 <p className='p-description'>
-                    Hello, My name is Ramadevi, and I'm an experienced developer focused on Front End Web Development. In my projects, I'm always writing clean and maintainable code, optimized for all sorts of browsers, and responsive for all sorts of devices. I'm a team player and have great experience working remotely with other developers from all around the world. I'm constantly improving my knowledge about the latest trends in programming, trying out new stuff, and contributing to open source.
+                    Hello, My name is Ramadevi, and I'm an experienced developer focused on Front End Web Development. In my projects, I'm always writing clean code, maintainable code and review pull request's, optimized for all browsers like Chrome, MSEdge, Firefox, IE, Safari, Opera and responsive for all devices(Phone, Tablet, Desktop). I'm a team player and have good experience working remotely with other developers in the team. I'm improving my knowledge about the latest versions in programming, trying out new stuff, and contributing to open source.
                 </p>
        </div>
     );
